045fe3053f1a8684c5c12c0f19404ae26524491e
Oskred <gogetenks.gogito@gmail.com>
2023-06-08 00:06:56 -0500

    Adding NavBar component

---
+++ sid-portfolio/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,24 @@
+import './NavBar.css';
+export const NavBar = () => {
+  return (
+    <>
+      <ul className="top-nav-links">
+        <li>
+          <a href="#Home">Home</a>
+        </li>
+        <li>
+          <a href="#About">About</a>
+        </li>
+        <li>
+          <a href="#SkillsAndCerts">Skills And Certs</a>
+        </li>
+        <li>
+          <a href="#Experience">Experience</a>
+        </li>
+        <li>
+          <a href="#Contact">Contact</a>
+        </li>
+      </ul>
+    </>
+  );
+};
