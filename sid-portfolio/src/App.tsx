e1569ee9b32d3a18576101c836160da1cd6bd3e2
Oskred <gogetenks.gogito@gmail.com>
2023-06-07 23:29:17 -0500

    Initial commit - Setting up

---
+++ sid-portfolio/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Portfolio</h1>
+      <title>Sid's Portfolio </title>
+    </>
+  )
+}
+
+export default App
