fc39d8371caab97c03cd91fefff8da2442c2bc52
Oskred <gogetenks.gogito@gmail.com>
2023-06-08 01:07:19 -0500

    Adding Introduction text

--- sid-portfolio/src/App.tsx
+++ sid-portfolio/src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css'
+import { AboutMe } from './components/AboutMe/AboutMe'
 import { NavBar } from './components/NavBar/NavBar'
 
 function App() {
 
   return (
     <>
-    <NavBar/>
-      
-      <h1>Portfolio</h1>
+      <NavBar/>
+      <div className="introduction">
+        <h1 className="intro-text"> Hello World</h1>
+        <h1 className="intro-text">Meet Siddarth!</h1>
+      </div>
       <title>Sid's Portfolio </title>
+      <AboutMe/>
     </>
   )
 }
