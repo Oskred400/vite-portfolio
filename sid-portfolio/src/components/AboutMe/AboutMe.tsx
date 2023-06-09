f76c6598808673a9ab9cef85e39b87c0061b06fd
Oskred <gogetenks.gogito@gmail.com>
2023-06-09 17:45:45 -0500

    Reffining and removing p tags

--- sid-portfolio/src/components/AboutMe/AboutMe.tsx
+++ sid-portfolio/src/components/AboutMe/AboutMe.tsx
@@ -9,12 +9,13 @@ export const AboutMe = () => {
             <div className="about-me" id="about">
             <h2>About Me</h2>
             <h6> Software Engineer | Software Developer </h6>
-            <p>  Siddarth is an insightful Software engineer passionate about all areas of software development and  </p>
-              <p> committed to developing practical and time-saving software solutions. </p>
+              <div className="professional-summary">
+                Siddarth is an insightful Software engineer passionate about all areas of software development and  
+               committed to developing practical and time-saving software solutions. 
 
-              <p> He has extensive experience in collaborating with multiple teams to deliver cost-effective solutions on time. </p>
-             <p>   He is enthusiastic about learning and adapting to new technologies. 
-            </p>
+               He has extensive experience in collaborating with multiple teams to deliver cost-effective solutions on time. 
+                He is enthusiastic about learning and adapting to new technologies. 
+                </div>
             <button onClick={onResumeClick} > Resume </button>
             </div>
         </>
