4be155b21430da7eb7ad70957234987cf5921c64
Oskred <gogetenks.gogito@gmail.com>
2023-06-09 17:43:24 -0500

    Refining more

--- sid-portfolio/src/components/AboutMe/AboutMe.tsx
+++ sid-portfolio/src/components/AboutMe/AboutMe.tsx
@@ -8,7 +8,13 @@ export const AboutMe = () => {
         <>
             <div className="about-me" id="about">
             <h2>About Me</h2>
-            <p> Software Engineer | Software Developer </p>
+            <h6> Software Engineer | Software Developer </h6>
+            <p>  Siddarth is an insightful Software engineer passionate about all areas of software development and  </p>
+              <p> committed to developing practical and time-saving software solutions. </p>
+
+              <p> He has extensive experience in collaborating with multiple teams to deliver cost-effective solutions on time. </p>
+             <p>   He is enthusiastic about learning and adapting to new technologies. 
+            </p>
             <button onClick={onResumeClick} > Resume </button>
             </div>
         </>
